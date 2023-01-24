4ba8ce083dd3d63a1333edf89fdd96c3b362ad19
Jacob Aagaard <jaa@it-minds.dk>
2023-01-24 16:18:57 +0100

    Add project information tests

---
+++ src/__mocks__/projectInformation.mock.ts
@@ -0,0 +1,38 @@
+import { GraphQLError } from 'graphql'
+import { MockedResponse } from '@apollo/client/testing'
+import { GetSingleProjectDocument, UpdateProjectDocument } from '../dataAccess'
+import getSingleProjectResponse from './getSingleProject'
+import updateProjectResponse from './updateProject'
+
+const projectId = getSingleProjectResponse.data.projects[0].id
+
+export const projectInformationMock: MockedResponse[] = [
+  {
+    request: {
+      query: GetSingleProjectDocument,
+      variables: { id: projectId },
+    },
+    result: getSingleProjectResponse,
+  },
+  {
+    request: {
+      query: UpdateProjectDocument,
+      variables: {
+        name: '',
+        projectId: null,
+        client: null,
+        address: null,
+        city: null,
+        country: null,
+        domain: null,
+        metaFields: null,
+        file: null,
+        id: projectId,
+      },
+    },
+    result: {
+      ...updateProjectResponse,
+      errors: [new GraphQLError('Test Error Message')],
+    },
+  },
+]
